00f070a3dcdaadcf7036bb86d4d344defdf7ecdc
jochemmpjes <jochemlel@hotmail.com>
2017-06-23 16:49:45 +0200

    inv

---
+++ Source/InventoryC/Assets/InventoryClick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryClick : MonoBehaviour {
+    public bool boll;
+    public Transform item;
+    public Transform canvas;
+
+    void Update()
+    {
+        if (boll == true)
+        {
+
+            item.position = Input.mousePosition;
+        }
+
+
+    }
+    public void CLick(Transform t)
+    {
+        if (item == null)
+        {
+            if (t.childCount > 0)
+            {
+                item = t.GetChild(0);
+                item.SetParent(canvas);
+                boll = true;
+            }
+        }
+        else
+        {
+            if (t.childCount > 0)
+            {
+                item.SetParent(t);
+                item.position = t.position;
+                item = t.GetChild(0);
+                item.SetParent(canvas);
+            }
+            else
+            {
+                item.SetParent(t);
+                item.position = t.position;
+                item = null;
+                boll = false;
+            }
+        }
+    }
+
+
+
+}
+
---
+++ Source/InventoryC/Assets/ItemSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSwitch : MonoBehaviour {
+    public Transform item;
+
+    public void CLick(Transform T)
+    {
+        if (T.childCount > 0)
+        {
+            item = T.GetChild(0);
+            item.SetParent(transform);
+            item.position = T.position;
+        }
+    }
+}
